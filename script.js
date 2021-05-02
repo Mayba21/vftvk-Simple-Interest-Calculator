09db0c776496ca041a223be702c79844c5048a87
Mayba21 <83539142+Mayba21@users.noreply.github.com>
2021-05-02 18:08:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-function compute()
+function compute();
+
 {
-    p = document.getElementById("principal").value;
-    
+ 
+ var principal = document.getElementById("principal").value;
+
+ if(principal<=0)
+
+
+ {
+
+alert('Please Enter a positive number greater than 0');
+document.getElementById("principal").focus();
+document.getElementById("principal").click();
+return null;
+
+ }
+
+var rate = document.getElementById("Rate").value;
+
+var years = document.getElementById("Years").value;
+
+var interest = principal*rate*years/100;
+
+console.log(P*R); 
+
+var ndate = new Date();
+
+newdate = ndate.getFullYear();
+
+newdate+= parseInt('Y')
+
+document.getElementById("result").InnerHTML - "<br>" + "<br>"
+
+
+function Ratechange()
+
+{
+  
+  var rchange = 
+  document.getElementById('Rate').value;
+  document.getElementById('rate-displayer').InnerHTML = rchange + '%';
 }
-        
+}
